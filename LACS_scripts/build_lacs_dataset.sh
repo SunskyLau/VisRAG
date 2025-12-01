320f8ccaec1d6b91a3bd386ff1af60a8fd384223
Yangtian Liu <530059237@qq.com>
2025-12-01 19:45:55 +0800

    fix: .sh

--- LACS_scripts/build_lacs_dataset.sh
+++ LACS_scripts/build_lacs_dataset.sh
@@ -4,7 +4,7 @@
 
 python LACS_scripts/build_lacs_dataset.py \
     --data_dir ./data/VisRAG-Ret-Train-In-domain-data \
-    --model_path openbmb/VisRAG \
+    --model_path openbmb/VisRAG-Ret \
     --output_path ./data/lacs_train_dataset_test.jsonl \
     --topk 5 \
     --batch_size 16 \
